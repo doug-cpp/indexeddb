5a73da791d395fe40f40ca43636214a953de1ca9
doug.cpp <doug.cpp@gmail.com>
2020-09-14 15:14:49 -0300

    Faz o tratamento de erros do crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,29 +21,9 @@ export class AppComponent {
 
   constructor(private idbs: IdbService) {
 
-    this.idbs.syncDb().then( _ => {
-      const jsonData = {
-        key: 'id012',
-        name: 'André Teixeira Sodré',
-        age: 23,
-        email: 'dedea@isso.com'
-      };
-      this.clients = this.idbs.readList('clients');
-      // OK!
-      this.idbs.create(jsonData, 'clients'). then(e => console.log(e, 'criado?'), er => console.log(er));
-      // OK!
-      // this.idbs.readObject('clients', 'id0039').then(o => console.log(o), err => console.error(err));
-      // OK!
-      // this.idbs.readList('clients').then(o => console.log(o), err => console.log(err));
-      // OK!
-      // this.idbs.update({
-        //   key: 'id003',
-        //   name: 'Outro camarada atualizado!',
-        //   age: 45,
-        //   email: 'outro@isso.com'
-        // }, 'clients', 'id003').then(o => console.log(o), err => console.log(err));
-        // this.idbs.delete('clients', 'id003').then(o => console.log(o), err => console.error(err));
-    });
+    // Colocar esse sync interno às operações?
+    // As operações sequentes já estão funcionando sem invocar o sync!
+    this.idbs.syncDb().then( _ => this.clients = this.idbs.readList('clients'));
 
     // NOT OK OUTSIDE!
     // this.idbs.readObject('clients', 'id003').then(o => console.log(o));
@@ -51,19 +31,14 @@ export class AppComponent {
 
   // ---------------------------------------------------------------------------
 
-  ngOnInit(): void {
-    // this.idbs.syncDb().then( _ => 
-    // );
-  }
-
-  openAdd() {
+  openAdd(): void {
     this.currentClient.key = '';
     this.currentClient.name = '';
     this.currentClient.age = 0;
     this.currentClient.email = '';
   }
 
-  add() {
+  add(): void {
     if(this.currentClient.key === ''
     && this.currentClient.name.length > 5
     && this.currentClient.email.length > 5) {
@@ -72,7 +47,8 @@ export class AppComponent {
         this.clients = this.idbs.readList('clients');
         this.openAdd(); // Limpa o form.
         document.getElementById('formdiv').hidden = true;
-      });
+      },
+      err => alert(err));
     }
     else if(this.currentClient.key.length > 4
     && this.currentClient.name.length > 5
@@ -81,14 +57,15 @@ export class AppComponent {
         this.clients = this.idbs.readList('clients');
         this.openAdd(); // Limpa o form.
         document.getElementById('formdiv').hidden = true;
-      });
+      },
+      err => alert(err));
     }
     else {
       alert('Dados inválidos no form');
     }
   }
 
-  edit(key: string) {
+  edit(key: string): void {
     this.idbs.readObject('clients', key).then((obj: Client) => {
       this.currentClient.key = obj.key;
       this.currentClient.name = obj.name;
@@ -97,7 +74,9 @@ export class AppComponent {
     });
   }
 
-  delete(key: string) {
-    alert('Altera cliente ' + key);
+  delete(key: string): void {
+    this.idbs.delete('clients', key);
+    this.clients = this.idbs.readList('clients');
+    document.getElementById('formdiv').hidden = true;
   }
 }
